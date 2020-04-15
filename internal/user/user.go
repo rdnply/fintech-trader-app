5f6ace05e631920022b3b39fac9161c97d4f4fa5
Radik12 <polyanscky.rodion@ya.ru>
2020-04-15 16:41:13 +0300

    Change struct for birthday

--- internal/user/user.go
+++ internal/user/user.go
@@ -6,9 +6,10 @@ import (
 	"time"
 )
 
+const DateLayout = "2006-01-02"
 
-type BirthdayDate struct {
-	Date time.Time
+type BirthDay struct {
+	time.Time
 }
 
 type JSONTime struct {
@@ -16,29 +17,51 @@ type JSONTime struct {
 }
 
 type User struct {
-	ID        int          `json:"id,omitempty"`
-	FirstName string       `json:"first_name,omitempty"`
-	LastName  string       `json:"last_name,omitempty"`
-	Birthday  BirthdayDate `json:"birthday,omitempty"`
-	Email     string       `json:"email"`
-	Password  string       `json:"password,omitempty"`
-	UpdatedAt JSONTime     `json:"updated_at,omitempty"`
-	CreatedAt JSONTime     `json:"created_at,omitempty"`
+	ID        int      `json:"id,omitempty"`
+	FirstName string   `json:"first_name,omitempty"`
+	LastName  string   `json:"last_name,omitempty"`
+	Birthday  BirthDay `json:"birthday,omitempty"`
+	Email     string   `json:"email"`
+	Password  string   `json:"password,omitempty"`
+	UpdatedAt JSONTime `json:"updated_at,omitempty"`
+	CreatedAt JSONTime `json:"created_at,omitempty"`
 }
 
-func (b *BirthdayDate) UnmarshalJSON(data []byte) error {
+func (b *BirthDay) UnmarshalJSON(data []byte) error {
 	s := string(data)
 	if s == "null" || s == `""` {
 		return nil
 	}
 
-	const layout = `"` + "2006-01-02" + `"`
+	const layout = `"` + DateLayout + `"`
 	t, err := time.Parse(layout, s)
 	if err != nil {
 		return fmt.Errorf("can't parse birth date string: %v", err)
 	}
-	b.Date = t
+	b.Time = t
 
+	return nil
+}
+
+func (bd *BirthDay) MarshalJSON() ([]byte, error) {
+	s := bd.Format(DateLayout)
+
+	return []byte(s), nil
+}
+
+func (bd BirthDay) Value() (driver.Value, error) {
+	return bd.Time, nil
+}
+
+func (bd *BirthDay) Scan(value interface{}) error {
+	t := value.(time.Time)
+	str := t.Format(DateLayout)
+	t, err := time.Parse(DateLayout, str)
+	if err != nil {
+		return fmt.Errorf("can't parse birth date in scanner for database: %v", err)
+	}
+
+	bd.Time = t
 	return nil
 }
 
